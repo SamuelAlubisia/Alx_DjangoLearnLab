88201f7049d2be378175d36fb215e71b4aec6e07
SamuelAlubisia <salubisia@gmail.com>
2025-02-23 14:56:55 +0300

    initial

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,29 +8,15 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian # type: ignore
 import sys
 
-def get_books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
-    if author:
-        books = Book.objects.filter(author=author)
-        print([book.title for book in books])
-    else:
-        print("Author not found.")
 
-def get_books_in_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
-        books = library.books.all()
-        print([book.title for book in books])
-    else:
-        print("Library not found.")
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
 def get_librarian_for_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
-        librarian = Librarian.objects.filter(library=library).first()
-        if librarian:
-            print(librarian.name)
-        else:
-            print("Librarian not found.")
-    else:
-        print("Library not found.")
+    library = Library.objects.get(name=library_name)
+    return library.librarian
