0c3a44340336b8c642e0bcb7805ae6b3253a3e9b
SamuelAlubisia <salubisia@gmail.com>
2025-02-23 17:19:55 +0300

    initial

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and getattr(user.userprofile, 'role', '') == 'Admin'
+    if not user.is_authenticated:
+        return False
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_dashboard.html')
+    return HttpResponse(f"User: {request.user.username}, Role: {request.user.userprofile.role}")
