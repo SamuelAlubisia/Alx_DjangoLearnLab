7297d0a1375dd0781d07a1e3f97ffb0d03d5b759
SamuelAlubisia <salubisia@gmail.com>
2025-02-23 16:04:22 +0300

    initial

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ def book_list(request):
     context = {'book_list': books}  
     return render(request, 'relationship_app/list_books.html', context)
 
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 class LibraryDetailView(DetailView):
     model = Library
